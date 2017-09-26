757c5f1c0659a2536548c3af7cf72398262f9ddc
Daniel <janzon.daniel@gmail.com>
2017-09-26 10:38:04 +0200

    create components - Lesson 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<app-servers></app-servers>`,
   styleUrls: ['./app.component.css']
+  /*
+  styles: [`
+      h3 {
+          color: #ff9492
+      }
+  `]
+  */
 })
 export class AppComponent {
 
