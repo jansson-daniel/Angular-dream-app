d2bdf15dfaaa6c67d5257ab07f5ca85ca7963cbe
dhanisthlm <dhanisthlm@gmail.com>
2017-09-26 21:58:50 +0200

    ngClass directive property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -19,6 +19,6 @@ export class ServerComponent {
     }
 
     getServerStatus () {
-        return this.serverStatus;
+        return this.serverStatus === 'online' ? this.serverStatus : '';
     }
 };
